f2f5b7bba7de8b30e2136e4aa6b6db1f605d0fab
Ryan J. Parker <ryan.jos.parker@gmail.com>
2023-01-18 16:55:14 -0800

    all tests passing

--- src/components/ShoppingList/ShoppingListForm.test.jsx
+++ src/components/ShoppingList/ShoppingListForm.test.jsx
@@ -9,16 +9,13 @@ describe('ShoppingListForm', () => {
 
   it('renders a form', () => {
     render(<ShoppingListForm id="test" />);
-
     const form = screen.getByTestId('shopping-list-form-test');
-
+    
     expect(form).toBeInTheDocument();
   });
 
   it('input field is operational', () => {
-
     render(<ShoppingListForm id="test" />);
-
     const input = screen.getByTestId('shopping-list-form-name-test');
     fireEvent.change(input, { target: { value: 'testing, attention please' } });
     
@@ -27,7 +24,6 @@ describe('ShoppingListForm', () => {
 
   it('submits form with shopping list on button click', () => {
     const onSubmit = jest.fn();
-    
     const defaultShoppingList = {
       id: null,
       name: 'shopping list',
@@ -37,13 +33,12 @@ describe('ShoppingListForm', () => {
     render(
       <ShoppingListForm id="test" onSubmit={onSubmit} />
     );
-
     const input = screen.getByTestId('shopping-list-form-name-test');
     fireEvent.change(input, { target: { value: 'shopping list' } });
 
     const button = screen.getByTestId('shopping-list-form-submit-button-test');
     fireEvent.click(button);
-
+    
     expect(onSubmit).toHaveBeenCalledWith(defaultShoppingList);
   });
 });
